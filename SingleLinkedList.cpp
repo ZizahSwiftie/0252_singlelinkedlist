7b1e430f5f20c11015c20f3688efd6ae387af0b2
ZizahSwiftie <azizahulnha@gmail.com>
2025-05-08 10:11:49 +0700

    commit 1

---
+++ SingleLinkedList.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Node {
+    public:
+    int noMhs;
+    Node *next;
+};
+
+class List 
+{
+    Node *START;
+
+    public:
+    List() {
+        START = NULL;
+    }
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan nomor mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if ((START == NULL) || (nim <= START->noMhs))
+        { 
+            if ((START != NULL) && (nim == START->noMhs)) 
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            return; 
+        }
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (current->noMhs < nim)) 
+        {
+            if (nim == current->noMhs) 
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        }
+    }
+}
