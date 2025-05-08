c5be44c633a45c4a4e9414da34e9ba96bda57806
ZizahSwiftie <azizahulnha@gmail.com>
2025-05-08 10:50:17 +0700

    commit 5

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -69,4 +69,39 @@ class LinkedList
         }
         return (*current != NULL);
     }
+    
+public:
+bool delNode(int nim)
+{
+    Node *current, *previous;
+    if (!Search(nim, &previous, &current))
+        return false;
+
+    if (current == START)
+        START = START->next;
+    else
+        previous->next = current->next;
+
+    delete current;
+    return true;
+}
+
+void traverse()
+{
+    if (listEmpty())
+    {
+        cout << "\nList Kosong\n";
+    }
+    else
+    {
+        cout << "\nData didalam list adalah: \n";
+        Node *currentNode = START;
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << endl;
+            currentNode = currentNode->next;
+        }
+        cout << endl;
+    }
+}
 };
