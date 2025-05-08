3826d59effda9419351ea1e139c51ee20b4cedd6
ZizahSwiftie <azizahulnha@gmail.com>
2025-05-08 10:15:00 +0700

    commit 2

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -45,6 +45,15 @@ class List
                 cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
             }
+            previous = current;
+            current = current->next;
         }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+
+    bool listEmpty() 
+    {
+        
     }
 }
